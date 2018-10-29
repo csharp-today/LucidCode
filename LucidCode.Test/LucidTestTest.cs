4eb9276f6ec63e9d05d7c764ca9f46ac65a1a4a0
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2018-10-29 22:59:53 +0100

    LucidTest - add Arrange method and tests

---
+++ LucidCode.Test/LucidTestTest.cs
@@ -0,0 +1,36 @@
+﻿using Shouldly;
+using Xunit;
+using static LucidCode.LucidTest;
+
+namespace LucidCode.Test
+{
+    public class LucidTestTest
+    {
+        [Fact]
+        public void Arrange_Returns_Act_Parameters()
+        {
+            // Arrange
+            const string ExpectedActParameter = "expected";
+
+            // Act
+            var actParameter = DefineExpected("someValue")
+                .Arrange(expected => ExpectedActParameter);
+
+            // Assert
+            actParameter.ShouldBe(ExpectedActParameter);
+        }
+
+        [Fact]
+        public void DefineExpected_Returns_Value()
+        {
+            // Arrange
+            var expectedDefinition = new { Value1 = "test", Value2 = "other" };
+
+            // Act
+            var definition = DefineExpected(expectedDefinition);
+
+            // Assert
+            definition.ShouldBe(expectedDefinition);
+        }
+    }
+}
--- LucidCode/LucidTest.cs
+++ LucidCode/LucidTest.cs
@@ -1,16 +1,28 @@
-﻿namespace LucidCode
+﻿using System;
+
+namespace LucidCode
 {
     /// <summary>
-    /// Support for AAA unit test pattern (Arrange, Act, Assert)
+    /// Syntax sugar for AAA unit test pattern (Arrange, Act, Assert)
     /// </summary>
     public static class LucidTest
     {
         /// <summary>
-        /// Define expected value for the assertion
+        /// Define expected value for the Assertion
         /// </summary>
-        /// <typeparam name="T">Type of expected object. Use anonymous type for multiple values</typeparam>
-        /// <param name="expected">Expected value</param>
-        /// <returns>Expected value</returns>
+        /// <typeparam name="T">Type of expected value(s) object. Use anonymous type for multiple values</typeparam>
+        /// <param name="expected">Expected value(s)</param>
+        /// <returns>Expected value(s)</returns>
         public static T DefineExpected<T>(T expected) => expected;
+
+        /// <summary>
+        /// Gets expected value(s) and prepare all Arrange actions
+        /// </summary>
+        /// <typeparam name="TExpected">Type of expected value(s)</typeparam>
+        /// <typeparam name="TParams">Type of parameter(s) for Act</typeparam>
+        /// <param name="expected">Expected value(s)</param>
+        /// <param name="arrangeFunc">Arrange function</param>
+        /// <returns>Act parameter(s)</returns>
+        public static TParams Arrange<TExpected, TParams>(this TExpected expected, Func<TExpected, TParams> arrangeFunc) => arrangeFunc(expected);
     }
 }
