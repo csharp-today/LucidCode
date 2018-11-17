6fbcd0529b0509e8a9297c1a0f5d4b8bbd928785
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2018-11-17 20:42:34 +0100

    Improve Act tests

--- LucidCode.Test/LucidTests/ActTest.cs
+++ LucidCode.Test/LucidTests/ActTest.cs
@@ -7,15 +7,21 @@ namespace LucidCode.Test.LucidTests
     public class ActTest
     {
         [Fact]
-        public void Result_Present_In_AssertManager()
+        public void Act_Provides_AssertManager()
         {
             // Arrange
             const string ExpectedResult = "result";
+            bool actExecuted = false;
 
             // Act
-            AssertManager<string> manager = LucidTest.Act(() => ExpectedResult);
+            AssertManager<string> manager = LucidTest.Act(() =>
+            {
+                actExecuted = true;
+                return ExpectedResult;
+            });
 
             // Assert
+            actExecuted.ShouldBeTrue();
             manager.ShouldNotBeNull();
             manager.ActResult.ShouldBe(ExpectedResult);
         }
