5e2f25fa328dfd357c9e36641cf8134e5a6f517f
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2018-11-29 01:41:39 +0100

    Migrate extension tests to LucidTest

--- LucidCode.Test/ExtensionsTest.cs
+++ LucidCode.Test/ExtensionsTest.cs
@@ -7,177 +7,89 @@ namespace LucidCode.Test
     public class ExtensionsTest
     {
         [Fact]
-        public void In_Negative()
-        {
-            // Act
-            var result = 5.In(1, 2, 3, 4, 6, 7, 8, 9);
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void In_Negative() => LucidTest
+            .Act(() => 5.In(1, 2, 3, 4, 6, 7, 8, 9))
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void In_Positive()
-        {
-            // Act
-            var result = 5.In(1, 2, 3, 5, 7, 9);
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void In_Positive() => LucidTest
+            .Act(() => 5.In(1, 2, 3, 5, 7, 9))
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void InArray_Negative()
-        {
-            // Act
-            var result = 5.In(new[] { 1, 2, 3 });
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void InArray_Negative() => LucidTest
+            .Act(() => 5.In(new[] { 1, 2, 3 }))
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void InArray_Positive()
-        {
-            // Act
-            var result = 5.In(new[] { 5 });
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void InArray_Positive() => LucidTest
+            .Act(() => 5.In(new[] { 5 }))
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void InCollection_Negative()
-        {
-            // Arrange
-            IEnumerable<int> collection = new[] { 1, 2, 3 };
-
-            // Act
-            var result = 5.In(collection);
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void InCollection_Negative() => LucidTest
+            .Arrange(() => (IEnumerable<int>)new[] { 1, 2, 3 })
+            .Act(collection => 5.In(collection))
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void InCollection_Positive()
-        {
-            // Arrange
-            IEnumerable<int> collection = new[] { 5 };
-
-            // Act
-            var result = 5.In(collection);
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void InCollection_Positive() => LucidTest
+            .Arrange(() => (IEnumerable<int>)new[] { 5 })
+            .Act(collection => 5.In(collection))
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void IsNotNull_Negative()
-        {
-            // Act
-            object obj = null;
-            var result = obj.IsNotNull();
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void IsNotNull_Negative() => LucidTest
+            .Arrange(() => (object)null)
+            .Act(obj => obj.IsNotNull())
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void IsNotNull_Positive()
-        {
-            // Act
-            var result = new object().IsNotNull();
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void IsNotNull_Positive() => LucidTest
+            .Act(() => new object().IsNotNull())
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void IsNull_Negative()
-        {
-            // Act
-            var result = new object().IsNull();
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void IsNull_Negative() => LucidTest
+            .Act(() => new object().IsNull())
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void IsNull_Positive()
-        {
-            // Act
-            object obj = null;
-            var result = obj.IsNull();
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void IsNull_Positive() => LucidTest
+            .Arrange(() => (object)null)
+            .Act(obj => obj.IsNull())
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void NotInArray_Negative()
-        {
-            // Act
-            var result = 5.NotIn(new[] { 5 });
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void NotInArray_Negative() => LucidTest
+            .Act(() => 5.NotIn(new[] { 5 }))
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void NotInArray_Positive()
-        {
-            // Act
-            var result = 5.NotIn(new[] { 1, 2, 3 });
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void NotInArray_Positive() => LucidTest
+            .Act(() => 5.NotIn(new[] { 1, 2, 3 }))
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void NotInCollection_Negative()
-        {
-            // Arrange
-            IEnumerable<int> collection = new[] { 5 };
-
-            // Act
-            var result = 5.NotIn(collection);
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void NotInCollection_Negative() => LucidTest
+            .Arrange(() => (IEnumerable<int>)new[] { 5 })
+            .Act(collection => 5.NotIn(collection))
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void NotInCollection_Positive()
-        {
-            // Arrange
-            IEnumerable<int> collection = new[] { 1, 2, 3 };
-
-            // Act
-            var result = 5.NotIn(collection);
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void NotInCollection_Positive() => LucidTest
+            .Arrange(() => (IEnumerable<int>)new[] { 1, 2, 3 })
+            .Act(collection => 5.NotIn(collection))
+            .Assert(result => result.ShouldBeTrue());
 
         [Fact]
-        public void NotIn_Negative()
-        {
-            // Act
-            var result = 5.NotIn(1, 2, 3, 4, 5);
-
-            // Assert
-            result.ShouldBeFalse();
-        }
+        public void NotIn_Negative() => LucidTest
+            .Act(() => 5.NotIn(1, 2, 3, 4, 5))
+            .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void NotIn_Positive()
-        {
-            // Act
-            var result = 5.NotIn(1, 2, 3, 4);
-
-            // Assert
-            result.ShouldBeTrue();
-        }
+        public void NotIn_Positive() => LucidTest
+            .Act(() => 5.NotIn(1, 2, 3, 4))
+            .Assert(result => result.ShouldBeTrue());
     }
 }
