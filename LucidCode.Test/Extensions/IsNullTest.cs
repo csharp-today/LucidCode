a9968fdcaf681f28534092f45f902d34cf9cee78
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-01-06 01:24:18 +0100

    Add tests for IsNull extension method

--- LucidCode.Test/Extensions/IsNullTest.cs
+++ LucidCode.Test/Extensions/IsNullTest.cs
@@ -6,14 +6,25 @@ namespace LucidCode.Test.Extensions
     public class IsNullTest
     {
         [Fact]
-        public void IsNull_Negative() => LucidTest
+        public void IsNull_Class_Negative() => LucidTest
             .Act(() => new object().IsNull())
             .Assert(result => result.ShouldBeFalse());
 
         [Fact]
-        public void IsNull_Positive() => LucidTest
+        public void IsNull_Class_Positive() => LucidTest
             .Arrange(() => (object)null)
             .Act(obj => obj.IsNull())
+            .Assert(result => result.ShouldBeTrue());
+
+        [Fact]
+        public void IsNull_Struct_Negative() => LucidTest
+            .Act(() => ((int?)123).IsNull())
+            .Assert(result => result.ShouldBeFalse());
+
+        [Fact]
+        public void IsNull_Struct_Positive() => LucidTest
+            .Arrange(() => (int?)null)
+            .Act(value => value.IsNull())
             .Assert(result => result.ShouldBeTrue());
     }
 }
