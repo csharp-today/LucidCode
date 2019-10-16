68e5345d9873a1a817dc0475cbcf728e2cc0e6db
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-10-16 18:10:30 +0200

    LightAssertManager extensions

---
+++ source/LucidCode/LucidTestExtensions.LightAssertManager.cs
@@ -0,0 +1,17 @@
+﻿using LucidCode.LucidTestFundations;
+using System;
+using System.Threading.Tasks;
+
+namespace LucidCode
+{
+    public static partial class LucidTestExtensions
+    {
+        /// <summary>
+        /// Execute Assert step
+        /// </summary>
+        /// <param name="manager">Assert manager</param>
+        /// <param name="assertAction">Assert action</param>
+        public static async Task AssertAsync(this Task<LightAssertManager> manager, Action assertAction) =>
+            (await manager).Assert(assertAction);
+    }
+}
