5a5f7a116433974cdee88d8a957383a468e5f182
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-10-18 18:39:08 +0200

    AssertManager extensions

---
+++ source/LucidCode/LucidTestExtensions.AssertManager.cs
@@ -0,0 +1,16 @@
+﻿using LucidCode.LucidTestFundations;
+using System;
+using System.Threading.Tasks;
+
+namespace LucidCode
+{
+    public static partial class LucidTestExtensions
+    {
+        /// <summary>
+        /// Execute Assert step
+        /// </summary>
+        /// <param name="assertAction">Assert action</param>
+        public static async Task AssertAsync<TResult>(this Task<AssertManager<TResult>> manager, Action<TResult> assertAction) =>
+            (await manager).Assert(assertAction);
+    }
+}
