9cb92ed549ae280877225ffecee889ae4eec3201
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-10-24 23:47:54 +0200

    Add missing Act Manager extensions

--- source/LucidCode/LucidTestExtensions.ActManager.cs
+++ source/LucidCode/LucidTestExtensions.ActManager.cs
@@ -14,15 +14,43 @@ namespace LucidCode
         /// <param name="manager">Act manager</param>
         /// <param name="actFunc">Act function</param>
         /// <returns>Manager for Assert step</returns>
-        public static async Task<AssertManager<TResult>> ActAsync<TActParameter, TResult>(this Task<ActManager<TActParameter>> manager, Func<TActParameter, TResult> actFunc) =>
+        public static async Task<AssertManager<TResult>> ActAsync<TActParameter, TResult>(
+            this Task<ActManager<TActParameter>> manager,
+            Func<TActParameter, TResult> actFunc) =>
             (await manager).Act(actFunc);
 
         /// <summary>
         /// Execute Act step
         /// </summary>
+        /// <param name="manager">Act manager</param>
         /// <param name="actFunc">Act action</param>
         /// <returns>Manager for Assert step</returns>
-        public static async Task<LightAssertManager> ActAsync<TActParameter>(this Task<ActManager<TActParameter>> manager, Action<TActParameter> actFunc) =>
+        public static async Task<LightAssertManager> ActAsync<TActParameter>(
+            this Task<ActManager<TActParameter>> manager,
+            Action<TActParameter> actFunc) =>
             (await manager).Act(actFunc);
+
+        /// <summary>
+        /// Execute Act step
+        /// </summary>
+        /// <typeparam name="TResult">Type of Act result. Use anonymous type for multiple values.</typeparam>
+        /// <param name="manager">Act manager</param>
+        /// <param name="actFunc">Act function</param>
+        /// <returns>Manager for Assert step</returns>
+        public static async Task<AssertManager<TResult>> ActAsync<TActParameter, TResult>(
+            this Task<ActManager<TActParameter>> manager,
+            Func<TActParameter, Task<TResult>> actFunc) =>
+            await (await manager).ActAsync(actFunc);
+
+        /// <summary>
+        /// Execute Act step
+        /// </summary>
+        /// <param name="manager">Act manager</param>
+        /// <param name="actFunc">Act action</param>
+        /// <returns>Manager for Assert step</returns>
+        public static async Task<LightAssertManager> ActAsync<TActParameter>(
+            this Task<ActManager<TActParameter>> manager,
+            Func<TActParameter, Task> actFunc) =>
+            await (await manager).ActAsync(actFunc);
     }
 }
